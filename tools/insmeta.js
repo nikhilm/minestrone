3229f2c80e2c228716639ab4af301f4220477c0f
Nikhil Marathe <nsm.nikhil@gmail.com>
2009-12-26 16:55:16 +0530

    Fixed requires, used songk

--- tools/insmeta.js
+++ tools/insmeta.js
@@ -1,12 +1,13 @@
 // node instmeta.js filename md5sum
 var sys = require("sys");
-var redis = require("./../deps/redis/redis");
-var metadata = require("./../deps/taglib/taglib").getFileTags;
+var redis = require("redis");
+var metadata = require("taglib").getFileTags;
+var songk = require("utils").songk;
 
 r = new redis.Client();
 r.connect( function() {
     var a = metadata(process.ARGV[2]);
     a.filename = process.ARGV[2];
     a.hash = process.ARGV[3];
-    r.set('song:'+process.ARGV[3], JSON.stringify(a)).addCallback( function() { r.close(); } );
+    r.set(songk(process.ARGV[3]), JSON.stringify(a)).addCallback( function() { r.close(); } );
 });
