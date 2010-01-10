7638c771f4583a7f352f63aa3fd2fa3a2672f278
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-01-10 23:27:38 +0530

    Added playlist.js ( empty )

---
+++ web/playlist.js
@@ -0,0 +1,10 @@
+var sys = require('sys')
+  , posix = require('posix')
+  , redis = require('redis')
+  , utils = require('utils')
+  , _ = require('underscore')._
+
+exports.add = function( id, cb ) {
+    utils.newRedis( function() {
+    });
+}
