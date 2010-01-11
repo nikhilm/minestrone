52c1a83164958c97ca9871e7096a051f1b4ff488
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-01-11 23:03:04 +0530

    Implemented song addition and membership query in playlist module

--- web/playlist.js
+++ web/playlist.js
@@ -1,10 +1,21 @@
 var sys = require('sys')
   , posix = require('posix')
   , redis = require('redis')
-  , utils = require('utils')
   , _ = require('underscore')._
 
-exports.add = function( id, cb ) {
-    utils.newRedis( function() {
+process.mixin( require('utils') );
+
+exports.add = function( id, song, cb ) {
+    newRedis( function() {
+        if( !song ) { cb(false); return ; }
+        this.redis.sadd( $pls(id), song ).addCallback( function(added) {
+            cb( added );
+        });
+    });
+}
+
+exports.member = function( plid, songid, cb ) {
+    newRedis( function() {
+        this.redis.sismember( $pls( plid ), songid ).addCallback(cb);
     });
 }
